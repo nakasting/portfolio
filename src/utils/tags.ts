7064d37e7c1af3e58d32daa76320ded890bef867
sting <bobadillasting@gmail.com>
2024-10-11 00:26:39 -0300

    Add new icons and tags for React, Spring, Material UI, PostgreSQL, React Router, Java, and TypeScript

---
+++ src/utils/tags.ts
@@ -0,0 +1,45 @@
+import ReactIcon from "../components/icons/ReactIcon.astro";
+import SpringIcon from "../components/icons/SpringIcon.astro";
+import MaterialUiIcon from "../components/icons/MaterialUiIcon.astro";
+import PostgreSQLIcon from "../components/icons/PostgreSQLIcon.astro";
+import ReactRouterIcon from "../components/icons/ReactRouterIcon.astro";
+import JavaIcon from "../components/icons/JavaIcon.astro";
+import TypeScriptIcon from "../components/icons/TypeScriptIcon.astro";
+
+export const TAGS = {
+    REACT: {
+        name: "React",
+        class: "bg-gray-500/20 text-white",
+        icon: ReactIcon,
+    },
+    SPRING: {
+        name: "Spring",
+        class: "bg-gray-500/20 text-white",
+        icon: SpringIcon,
+    },
+    MATERIAL_UI: {
+        name: "Material UI",
+        class: "bg-gray-500/20 text-white",
+        icon: MaterialUiIcon,
+    },
+    POSTGRE_SQL: {
+        name: "PostgreSQL",
+        class: "bg-gray-500/20 text-white",
+        icon: PostgreSQLIcon,
+    },
+    REACT_ROUTER: {
+        name: "React Router",
+        class: "bg-gray-500/20 text-white",
+        icon: ReactRouterIcon,
+    },
+    JAVA: {
+        name: "Java",
+        class: "bg-gray-500/20 text-white",
+        icon: JavaIcon,
+    },
+    TYPESCRIPT: {
+        name: "TypeScript",
+        class: "bg-gray-500/20 text-white",
+        icon: TypeScriptIcon,
+    },
+};
