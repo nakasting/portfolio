cd3f6fd542279464b05fede4e5773a9c6b8469f6
sting <bobadillasting@gmail.com>
2024-10-13 20:17:18 -0300

    Refactor tags.ts to update tag colors

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -9,37 +9,37 @@ import TypeScriptIcon from "../components/icons/TypeScriptIcon.astro";
 export const TAGS = {
     REACT: {
         name: "React",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: ReactIcon,
     },
     SPRING: {
         name: "Spring",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: SpringIcon,
     },
     MATERIAL_UI: {
         name: "Material UI",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: MaterialUiIcon,
     },
     POSTGRE_SQL: {
         name: "PostgreSQL",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: PostgreSQLIcon,
     },
     REACT_ROUTER: {
         name: "React Router",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: ReactRouterIcon,
     },
     JAVA: {
         name: "Java",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: JavaIcon,
     },
     TYPESCRIPT: {
         name: "TypeScript",
-        class: "bg-gray-500/20 text-white",
+        class: "bg-zinc-800 text-white",
         icon: TypeScriptIcon,
     },
 };
